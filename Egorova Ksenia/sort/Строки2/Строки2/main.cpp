02c7ecb973960c281a598006a9dd5000d1e83226
EgorovaKsenia <73521644+EgorovaKsenia@users.noreply.github.com>
2021-05-23 12:55:36 +0300

    Add files via upload

---
+++ Egorova Ksenia/sort/Строки2/Строки2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "media_processing.h"
+using namespace std;
+
+int main() {
+	string request;
+	cin >> request;
+
+	Selection test;
+	test.load_from_file("input.txt");
+	test.process_request(request);
+	test.save_to_file("output.txt");
+
+	return 0;
+}
